3a135cb7d6746babf988de3c8c43eae45324bd52
ghostmoai <lospapusrandon@gmail.com>
2026-09-19 14:24:10 -0300

    chore: Purge non-essential legacy files and compiled binaries from git

--- iniciar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title EncryptorX - Motor Criptografico Nativo Rust v3.0.0
-cd /d "%~dp0"
-if exist "EncryptorX.exe" (
-    start "" "EncryptorX.exe"
-) else (
-    echo [ERROR] No se encontro EncryptorX.exe.
-    pause
-)
